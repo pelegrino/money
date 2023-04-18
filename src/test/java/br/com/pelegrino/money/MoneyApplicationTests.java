73e84cac31ece14c3c1798706ac9f88589b56cfb
Pelegrino <pelegrino@gmail.com>
2023-04-18 11:02:40 -0300

    Criando o Money

---
+++ src/test/java/br/com/pelegrino/money/MoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pelegrino.money;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
