9486d1f702fc416fb302f01d17624d0dbd0acb6f
Pelegrino <pelegrino@gmail.com>
2023-04-19 14:43:53 -0300

    Cadastro via Post

--- src/main/java/br/com/pelegrino/money/resource/CategoriaResource.java
+++ src/main/java/br/com/pelegrino/money/resource/CategoriaResource.java
@@ -1,14 +1,22 @@
 package br.com.pelegrino.money.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.pelegrino.money.model.Categoria;
 import br.com.pelegrino.money.repository.CategoriaRepository;
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/categorias")
@@ -22,4 +30,19 @@ public class CategoriaResource {
 		return categoriaRepository.findAll();
 	}
 
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+			.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public Optional<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+		return categoriaRepository.findById(codigo);
+	}
 }
